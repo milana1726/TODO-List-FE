8eb62e0e70c52e9597367695f39db6dc266eefb0
Melnychuk <milana1726@gmail.com>
2025-05-18 00:34:43 +0200

    feat: implement routing and not-found-page component

--- todolist-fe/src/app/app.routes.ts
+++ todolist-fe/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
+import { TodoPageComponent } from './todos/pages/todo-page/todo-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'todos',
+  },
+  {
+    path: 'todos',
+    component: TodoPageComponent,
+  },
+  { path: '**', component: NotFoundPageComponent },
+];
